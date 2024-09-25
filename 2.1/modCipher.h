e02fd3d11d75622ce487c6495c5ddad2691923bc
aleksey3sotni <belkov.alex2005@yandex.ru>
2024-09-25 11:19:19 +0300

    Update modCipher.h

--- 2.1/modCipher.h
+++ 2.1/modCipher.h
@@ -1,17 +1,19 @@
-#ifndef MODCIPHER_H
-#define MODCIPHER_H
-
+#pragma once
+#include <vector>
 #include <string>
-using namespace std;
-
-class modCipher {
-public:
-    modCipher(int skey);
-    string encrypt(string &text);
-    string decrypt(string &text);
-
+#include <map>
+#include <locale>
+class modAlphaCipher
+{
 private:
-    int key; 
+std::wstring numAlpha =L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; 
+std::map <char,int> alphaNum; 
+std::vector <int> key; 
+std::vector<int> convert(const std::wstring&s);
+std::wstring convert(const std::vector<int>&v);
+public:
+modAlphaCipher()=delete;
+modAlphaCipher(const std::wstring& skey); 
+std::wstring encrypt(const std::wstring& open_text);
+std::wstring decrypt(const std::wstring& cipher_text);
 };
-
-#endif 
